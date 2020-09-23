96c5230df75206b6889640106d3e1cd591cbb4dc
Pablo Rocha <jose.rocha3@gmail.com>
2020-09-23 18:24:40 -0500

    add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-66485739-17"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-66485739-17');            
+                `,
+            }}
+          />
+        </Head>
         <body>
           <ColorModeScript initialValue="dark" />
           <Main />
