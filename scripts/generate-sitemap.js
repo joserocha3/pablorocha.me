71f831b37805c6611a66d4d0dea1ee4826f6f0d6
Pablo Rocha <jose.rocha3@gmail.com>
2020-09-29 12:17:24 -0500

    update sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -3,34 +3,36 @@ const globby = require('globby')
 const prettier = require('prettier')
 
 const generateSitemap = async () => {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc')
-
-  // Ignore Next.js specific files (e.g., _app.js) and API routes.
   const pages = await globby([
-    'src/pages/**/*{.js,.mdx}',
+    'src/pages/**/*.js',
+    '_posts/**/*.md',
     '!src/pages/_*.js',
     '!src/pages/blog/[slug].js',
   ])
 
+  const paths = pages.map((page) => {
+    const path = page
+      .replace('src/pages', '')
+      .replace('/index', '')
+      .replace('.js', '')
+      .replace('_posts', '/blog')
+      .replace('.md', '')
+
+    return `
+      <url>
+        <loc>${`https://pablorocha.me${path}`}</loc>
+      </url>
+    `
+  })
+
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages
-          .map((page) => {
-            const path = page
-              .replace('src/pages', '')
-              .replace('.js', '')
-              .replace('.mdx', '')
-            const route = path.replace('/index', '')
-            return `
-                  <url>
-                    <loc>${`https://pablorocha.me${route}`}</loc>
-                  </url>
-                `
-          })
-          .join('')}
+      ${paths.join('')}
     </urlset>
   `
+
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc')
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
