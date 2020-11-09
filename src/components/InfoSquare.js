501547a474f10be00a6894df83794c111e6ad5bc
Pablo Rocha <jose.rocha3@gmail.com>
2020-11-09 15:06:08 -0600

    unwrap component

--- src/components/InfoSquare.js
+++ src/components/InfoSquare.js
@@ -18,28 +18,29 @@ const InfoSquare = ({
   const borderColor = useColorModeValue(darkColor, lightColor)
 
   return (
-    <WrapItem
-      as={Link}
-      display="flex"
-      _hover={{ textDecoration: 'none' }}
-      href={href}
-      isExternal
-      flexDirection="column"
-      borderTop="3px solid"
-      p={4}
-      borderRadius="md"
-      fontSize="lg"
-      maxW={300}
-      bg={bg}
-      borderColor={borderColor}
-      key={title}
-      h="100%"
-      {...rest}
-    >
-      <Heading as="h3" mb={6} fontSize="2xl">
-        {title}
-      </Heading>
-      <Text mb={6}>{description}</Text>
+    <WrapItem>
+      <Link
+        display="flex"
+        _hover={{ textDecoration: 'none' }}
+        href={href}
+        isExternal
+        flexDirection="column"
+        borderTop="3px solid"
+        p={4}
+        borderRadius="md"
+        fontSize="lg"
+        maxW={300}
+        bg={bg}
+        borderColor={borderColor}
+        key={title}
+        h="100%"
+        {...rest}
+      >
+        <Heading as="h3" mb={6} fontSize="2xl">
+          {title}
+        </Heading>
+        <Text mb={6}>{description}</Text>
+      </Link>
     </WrapItem>
   )
 }
