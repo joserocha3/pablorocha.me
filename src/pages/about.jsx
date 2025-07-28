d61c7685cfa4e0f7a79ed4ec205299e88d85bdcf
Pablo Rocha <jose.rocha3@gmail.com>
2025-07-28 16:43:00 -0500

    use the not future version

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import Image from 'next/future/image'
+import Image from 'next/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
@@ -11,8 +11,8 @@ import portraitImage from '@/images/portrait.jpg'
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
-      <Link href={href} target="_blank" className="group flex text-sm font-medium text-zinc-800 transition hover:text-sky-600 dark:text-zinc-200 dark:hover:text-sky-300">
-        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-sky-600 dark:group-hover:fill-sky-300" />
+      <Link href={href} target="_blank" className="flex text-sm font-medium transition group text-zinc-800 hover:text-sky-600 dark:text-zinc-200 dark:hover:text-sky-300">
+        <Icon className="flex-none w-6 h-6 transition fill-zinc-500 group-hover:fill-sky-600 dark:group-hover:fill-sky-300" />
         <span className="ml-4">{children}</span>
       </Link>
     </li>
@@ -45,13 +45,13 @@ export default function About() {
                 src={portraitImage}
                 alt="pablo rocha headshot"
                 sizes="(min-width: 1024px) 32rem, 20rem"
-                className="aspect-square rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                className="object-cover aspect-square rounded-2xl bg-zinc-100 dark:bg-zinc-800"
               />
             </div>
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">Hi! I’m Pablo Rocha 👋</h1>
-            <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <div className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
               <p>
                 I’m currently living in the southern part of Texas right along the border. I hop back and forth between working on client projects and focusing on a few exciting
                 ventures.
@@ -81,7 +81,7 @@ export default function About() {
               <SocialLink href="https://instagram.com/jprocha101" aria-label="Follow on Instagram" icon={InstagramIcon} className="mt-4">
                 Follow on Instagram
               </SocialLink>
-              <SocialLink href="mailto:pablo@pablorocha.me" icon={MailIcon} className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40">
+              <SocialLink href="mailto:pablo@pablorocha.me" icon={MailIcon} className="pt-8 mt-8 border-t border-zinc-100 dark:border-zinc-700/40">
                 pablo@pablorocha.me
               </SocialLink>
             </ul>
